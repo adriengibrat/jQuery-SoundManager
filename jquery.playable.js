7955831c697becd90cda8d8cafeae01c642f20e3
Adrien Gibrat <adrien.gibrat@gmail.com>
2010-03-21 18:51:23 -0700

    Corrected inArray testing ;)

--- jquery.playable.js
+++ jquery.playable.js
@@ -10,7 +10,7 @@
 	var sm = soundManager;
 	$.playable = $.extend( function( url, settings ) {
 		$.each( settings, function( key, value ) { // Parse settings to isolate SoundManager properties
-			if ( $.inArray( key, $.playable.properties ) ) {
+			if ( $.inArray( key, $.playable.properties ) != -1 ) {
 				sm[key] = (key == 'flash9Options' || key == 'movieStarOptions') ? $.extend(sm[key],value) : value;
 				delete settings[key]; // Unset propeties from settings
 			}
